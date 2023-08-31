2f3054625886a91997d1d7cdb2f51411d3227f24
anand269 <anand269@gmail.com>
2023-08-31 10:50:01 +0530

    one more commit 31st Aug

--- TestProject/src/Demo1.java
+++ TestProject/src/Demo1.java
@@ -4,6 +4,15 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo1 java");
+		long l = 2344243432L;
+		
+		float f = 2343.324f;
+		int num1=(int) (l+f);
+		System.out.println(num1);
+		
+		int num2=2112;
+		float f2=num2;
+		
 	}
 
 }
